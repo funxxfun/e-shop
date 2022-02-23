45144c29d67baa51f24bc953dfee8f3379e83807
funxxfun <23shirasawa@gmail.com>
2022-02-23 04:50:48 +0000

    [Add]ja.yml

--- config/application.rb
+++ config/application.rb
@@ -10,16 +10,13 @@ module EShop
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    # i18n
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.yml').to_s]
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-  end
-end
-
-module I18nApp
-  class Application < Rails::Application
-    config.i18n.default_locale = :ja # 追加
   end
 end
