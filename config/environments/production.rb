18ea1a78e27c1798636370238baeded5f0629eb1
funxxfun <23shirasawa@gmail.com>
2022-06-29 13:07:14 +0000

    [Change]production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,7 +71,7 @@ Rails.application.configure do
     domain:               'gmail.com',
     user_name:            ENV["ACTION_MAILER_USER"],
     password:             ENV["ACTION_MAILER_KEY"],
-    authentication:       'plain',
+    authentication:       'login',
     enable_starttls_auto:  true
    }
 
